397d0715b4abed6813f3b5b0b0129ff60965af55
sugandhi15 <sugandhibansal26@gmail.com>
2024-11-23 14:30:10 +0530

    added cors origin to all

--- huddleHub/settings.py
+++ huddleHub/settings.py
@@ -59,10 +59,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
+CORS_ALLOWED_ORIGINS = ["*"]
 
 ROOT_URLCONF = 'huddleHub.urls'
 
